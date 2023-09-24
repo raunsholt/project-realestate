a73e37c860c74c90677d0d701090e8bff6c3e777
Alexander Raunsholt <raunsholt@gmail.com>
2023-09-24 20:08:11 +0200

    old-int-ver1

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,12 +1,38 @@
 import fs from 'fs';
 import path from 'path';
 
+export default async function handler(req, res) {
+  const { address } = req.query;
+
+  try {
+      const addressResponse = await fetch(`https://api.dataforsyningen.dk/adresser?q=${address}`);
+      const addressData = await addressResponse.json();
+
+      if (addressData.length > 0) {
+          const adresseid = addressData[0].id;
+
+          const buildingResponse = await fetch(`https://api.dataforsyningen.dk/bbrlight/enheder?adresseid=${adresseid}`);
+          const buildingData = await buildingResponse.json();
+
+          if (buildingData.length > 0) {
+              res.status(200).json(buildingData);
+          } else {
+              res.status(404).json({ message: "No building data found for the specified address." });
+          }
+      } else {
+          res.status(404).json({ message: "Address not found." });
+      }
+  } catch (error) {
+      res.status(500).json({ message: "Internal Server Error", error: error.message });
+  }
+}
+
 function loadPolicy(policyType) {
   const filePath = path.join(process.cwd(), 'policies', `${policyType}.txt`);
   return fs.readFileSync(filePath, 'utf8');
 }
 
-export default async function (req, res) {
+/* export default async function (req, res) {
   const { question, policyType } = req.body; // get policyType from request
 
   if (!question) {
@@ -57,4 +83,4 @@ export default async function (req, res) {
       }
     });
   }
-}
+} */
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,161 @@
 import Head from "next/head";
 import { useState } from "react";
-import { ChakraProvider, Box, Image, Heading, Select, Spinner, Input, Button, Text } from "@chakra-ui/react";
+import { ChakraProvider, Box, Image, Heading, Input, Button, Radio, RadioGroup, Textarea, VStack, List, ListItem } from "@chakra-ui/react";
 
 export default function Home() {
-  const [userQuestion, setUserQuestion] = useState("");
-  const [result, setResult] = useState();
-  const [policyType, setPolicyType] = useState("car"); // default to car policy
-  const [isLoading, setIsLoading] = useState(false);
+  const [address, setAddress] = useState("");
+  const [textInput1, setTextInput1] = useState("");
+  const [textInput2, setTextInput2] = useState("");
+  const [textInput3, setTextInput3] = useState("");
+  const [radioValue, setRadioValue] = useState("");
+  const [dataField, setDataField] = useState("");
+  const [resultField, setResultField] = useState("");
 
-
-  async function onSubmit(event) {
+  async function onGetData(event) {
     event.preventDefault();
-    setIsLoading(true); // Set loading state to true when the request starts
+    const encodedAddress = encodeURIComponent(address);
     try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ question: userQuestion, policyType: policyType }), // send policy type
-      });
+        const response = await fetch(`/api/generate?address=${encodedAddress}`);
+        const buildingData = await response.json();
+        let insideArea = buildingData[0].BEBO_ARL;
+        let rooms = buildingData[0].VAERELSE_ANT;
+        let bathrooms = buildingData[0].AntBadevaerelser;
+        let toilets = buildingData[0].AntVandskylToilleter;
+        let build = buildingData[0].bygning.OPFOERELSE_AAR;
+        let reBuild = buildingData[0].bygning.OMBYG_AAR;
+        // let planes = buildingData[0].bygning.ETAGER_ANT;
+        let heating = buildingData[0].bygning.VARMEINSTAL_KODE;
+        let use = buildingData[0].ENH_ANVEND_KODE;
+        let wall = buildingData[0].bygning.YDERVAEG_KODE;
+        let roof = buildingData[0].bygning.TAG_KODE;
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
+        let roofTypes = {
+            "1": "Fladt tag",
+            "2": "Tagpap",
+            "3": "Fibercement",
+            "4": "Cementsten",
+            "5": "Tegl",
+            "6": "Metalplader",
+            "7": "Stråtag",
+            "10": "Fibercement",
+            "11": "PVC",
+            "12": "Glas"
+        };
 
-      setResult(data.result);
-      setUserQuestion("");
+        let wallTypes = {
+            "1": "Mursten",
+            "2": "Letbeton",
+            "3": "Fibercement",
+            "4": "Bindingsværk",
+            "5": "Træbeklædning",
+            "6": "Betonelementer",
+            "8": "Metalplader",
+            "10": "Fibercement",
+            "11": "PVC",
+            "12": "Glas"
+        };
+
+        let useTypes = {
+            "110": "Stuehus til landbrugsejendom",
+            "120": "Parcelhus",
+            "130": "Rækkehus",
+            "140": "Lejlighed",
+            "510": "Sommerhus",
+            "540": "Kolonihave"
+        };
+
+        let heatingTypes = {
+            "1": "Fjernvarme",
+            "2": "Centralvarme",
+            "3": "Ovne",
+            "5": "Varmepumpe",
+            "6": "Centralvarme med to fyringsenheder",
+            "7": "Elovne",
+            "8": "Gasradiatorer",
+            "9": "Ingen varmeinstallationer"
+        };
+
+        let buildingText = "";
+
+        if (use !== null && useTypes.hasOwnProperty(use)) {
+            use = useTypes[use];
+            buildingText += "Boligtype: " + use + ". ";
+        }
+
+        if (insideArea !== null) {
+            buildingText += "Boligareal: " + insideArea + ". ";
+        }
+
+        if (rooms !== null) {
+            if (rooms <= 2) {
+                buildingText += "Værelser i alt: " + rooms + ". ";
+            }
+            else {
+                rooms = rooms - 1;
+                buildingText += "Stue: 1. Værelser: " + rooms + ". ";
+            }
+        }
+
+        if (bathrooms !== null) {
+            buildingText += "Badeværelser: " + bathrooms + ". ";
+        }
+
+        if (toilets !== null && toilets > bathrooms) {
+            toilets = toilets - bathrooms;
+            buildingText += "Toiletter: " + toilets + ". ";
+        }
+
+        if (build !== null) {
+            buildingText += "Byggeår: " + build + ". ";
+        }
+
+        if (reBuild !== null && reBuild !== 0) {
+            buildingText += "Ombygget: " + reBuild + ". ";
+        }
+
+        if (roof !== null && roofTypes.hasOwnProperty(roof)) {
+            roof = roofTypes[roof];
+            buildingText += "Tag: " + roof + ". ";
+        }
+
+        if (wall !== null && wallTypes.hasOwnProperty(wall)) {
+            wall = wallTypes[wall];
+            buildingText += "Ydervægge: " + wall + ". ";
+        }
+
+        if (heating !== null && heatingTypes.hasOwnProperty(heating)) {
+            heating = heatingTypes[heating];
+            buildingText += "Varme: " + heating + ". ";
+        }
+
+        setDataField(buildingText); 
     } catch (error) {
-      console.error(error);
-      alert(error.message);
-    } finally {
-      setIsLoading(false); // Set loading state to false when the request completes (either success or error)
+        console.error("Error fetching building data:", error);
+    }
+}
+
+
+  async function onGenerateText(event) {
+    event.preventDefault();
+    // Handle the logic for the "Generate text" button here
+  }
+
+  const [suggestions, setSuggestions] = useState([]);
+
+  async function handleAddressChange(e) {
+    const query = e.target.value;
+    setAddress(query);
+
+    if (query.length > 2) { // Only fetch suggestions if query length is greater than 2
+      try {
+        const response = await fetch(`https://api.dataforsyningen.dk/adresser/autocomplete?q=${query}`);
+        const data = await response.json();
+        setSuggestions(data);
+      } catch (error) {
+        console.error("Error fetching address suggestions:", error);
+      }
+    } else {
+      setSuggestions([]); // Clear suggestions if query length is less than or equal to 2
     }
   }
 
@@ -41,42 +164,90 @@ export default function Home() {
       <Box as="div">
         <Head>
           <title>Boligtekst AI</title>
-          <link rel="icon" href="/monocle.png" /> {/* Consider changing this icon to something more relevant */}
+          <link rel="icon" href="/house.png" />
         </Head>
 
-        <Box as="main" display="flex" flexDirection="column" alignItems="center" pt="4" w="100%">
-          <Box maxWidth="600px" width="100%"> {/* This is the centered content block */}
-            <Image src="/monocle.png" w="34px" mx="auto" />
-            <Heading as="h3" size="lg" my="4" textAlign="center">Boligtekst AI</Heading>
-            <Text as="h4" mb="2">Vælg police</Text>
-            <form onSubmit={onSubmit}>
-              <Select value={policyType} onChange={(e) => setPolicyType(e.target.value)} mb="4">
-                <option value="car">Bil</option>
-                <option value="accident">Ulykke</option>
-              </Select>
-              <Text as="h4" mb="2">Skriv dit spørgsmål</Text>
+        <Box as="main" display="flex" flexDirection="column" alignItems="center" pt="6" w="100%" px="4">
+          <VStack spacing={6} maxWidth="600px" width="100%">
+            <Box textAlign="center">
+              <Image src="/house.png" w="34px" mx="auto" mb="2" />
+              <Heading as="h3" size="lg">Boligtekst AI</Heading>
+            </Box>
+
+            <form onSubmit={onGetData}>
+              <VStack spacing={4}>
+                <Box position="relative" width="100%">
+                  <Input
+                    type="text"
+                    placeholder="Enter address"
+                    value={address}
+                    onChange={handleAddressChange}
+                  />
+                  {suggestions.length > 0 && (
+                    <List position="absolute" top="100%" width="100%" bg="white" border="1px solid #ccc" zIndex="1">
+                      {suggestions.map((suggestion, index) => (
+                        <ListItem key={index} padding="1" cursor="pointer" onClick={() => {
+                          setAddress(suggestion.tekst);
+                          setSuggestions([]); // Clear suggestions after selecting an address
+                        }}>
+                          {suggestion.tekst}
+                        </ListItem>
+                      ))}
+                    </List>
+                  )}
+                </Box>
+                <Button type="submit" colorScheme="teal" width="full">Get data</Button>
+              </VStack>
+            </form>
+
+            <Textarea
+              placeholder="Editable data field"
+              value={dataField}
+              onChange={(e) => setDataField(e.target.value)}
+              size="md"
+            />
+
+            <VStack spacing={4} width="100%">
               <Input
                 type="text"
-                name="question"
-                placeholder="Fx: 'Dækker min forsikring X situation / Y person'"
-                value={userQuestion}
-                onChange={(e) => setUserQuestion(e.target.value)}
-                mb="4"
+                placeholder="Text input 1"
+                value={textInput1}
+                onChange={(e) => setTextInput1(e.target.value)}
               />
-              <Button type="submit" colorScheme="teal">Stil spørgsmål</Button>
+              <Input
+                type="text"
+                placeholder="Text input 2"
+                value={textInput2}
+                onChange={(e) => setTextInput2(e.target.value)}
+              />
+              <Input
+                type="text"
+                placeholder="Text input 3"
+                value={textInput3}
+                onChange={(e) => setTextInput3(e.target.value)}
+              />
+            </VStack>
+
+            <RadioGroup onChange={setRadioValue} value={radioValue}>
+              <VStack spacing={2} align="start">
+                <Radio value="option1">Option 1</Radio>
+                <Radio value="option2">Option 2</Radio>
+                <Radio value="option3">Option 3</Radio>
+              </VStack>
+            </RadioGroup>
+
+            <form onSubmit={onGenerateText}>
+              <VStack spacing={4}>
+                <Button type="submit" colorScheme="teal" width="full">Generate text</Button>
+                <Textarea
+                  placeholder="Editable result field"
+                  value={resultField}
+                  onChange={(e) => setResultField(e.target.value)}
+                  size="md"
+                />
+              </VStack>
             </form>
-            {isLoading && (
-              <Box display="flex" justifyContent="center" alignItems="center" mt="4">
-                <Spinner size="xl" speed="0.65s" emptyColor="gray.200" color="blue.500" />
-              </Box>
-            )}
-            {result && (
-              <Box mt="4" p="4" borderWidth="1px" borderRadius="md" bg="gray.50" w="100%">
-                <Heading as="h4" size="md" mb="2">Svar:</Heading>
-                <Text>{result}</Text>
-              </Box>
-            )}
-          </Box>
+          </VStack>
         </Box>
       </Box>
     </ChakraProvider>
